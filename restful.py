4d17980156377c024f87df23d9ff8e7c99f0d0ec
Rahul Barhate <rahulbarhate97@gmail.com>
2019-04-25 22:01:36 +0530

    Add files via upload

---
+++ restful.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+languages = [{'name' : 'Python'}, {'name' : 'C++'}, {'name' : 'Java'}]
+
+@app.route("/", methods=['GET'])
+def test():
+    return jsonify({'message' : 'It Works!'})
+
+@app.route("/lang", methods=['GET'])
+def returnAll():
+    return jsonify({'languages' : languages})
+
+@app.route("/lang/<string:name>", methods=['GET'])
+def returnOne(name):
+    langs = [x for x in languages if x['name']==name]
+    return jsonify({'name':langs[0]})
+
+@app.route("/lang", methods = ['POST'])
+def addOne():
+    language = {'name' : request.json['name']}
+    languages.append(language)
+    return jsonify({'languages': languages})
+
+if __name__=='__main__':
+    app.run(debug=True)
